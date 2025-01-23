84633ddacc934066a50053138d3d1d913f423257
Baeksnagjun <juny200@naver.com>
2025-01-23 14:40:12 +0900

    Initial commit

---
+++ app/src/androidTest/java/com/example/fe/ViewPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.fe
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ViewPagerAdapter(private val imageList: List<Int>) :
+    RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.image_view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_viewpager, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.imageView.setImageResource(imageList[position])
+    }
+
+    override fun getItemCount(): Int = imageList.size
+}
